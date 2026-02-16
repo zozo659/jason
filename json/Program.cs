2af4ea3d437ac855a3b8d14389d02b4a08618c83
Kalasziz <Kalasziz@310-08>
2026-02-16 10:17:24 +0100

    masodik feladat atlag szamitas

--- json/Program.cs
+++ json/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System.Diagnostics.Tracing;
+using System.Text.Json;
 
 namespace json
 {
@@ -8,6 +9,11 @@ namespace json
         public List<int> korok { get; set; }
 
     }
+        class Diak
+        {
+        public String nev { get; set; }
+        public List<int> jegyek { get; set; }
+    }
     internal class Program
     {
         static void Main(string[] args)
@@ -15,12 +21,30 @@ namespace json
             String fajl=File.ReadAllText("adatok.json",System.Text.Encoding.Latin1);
             Console.WriteLine(fajl);
             adat adattok = System.Text.Json.JsonSerializer.Deserialize<adat>(fajl);
-            foreach (var nev in adattok.nevek)
+            foreach (var nevee in adattok.nevek)
             {
-                Console.WriteLine(nev);
+                Console.WriteLine(nevee);
             }
             //első életkor
             Console.WriteLine($"{adattok.nevek[0]} életkora: {adattok.korok[0]}");
+
+            //diakok.json
+            fajl = File.ReadAllText("diakok.json", System.Text.Encoding.Latin1);
+            Console.WriteLine(fajl);
+            List<Diak> diakok =JsonSerializer.Deserialize<List<Diak>>(fajl);
+            Console.WriteLine("Keresett név:");
+            String neve = Console.ReadLine();
+            bool megvan = false;
+            foreach (var diak in diakok)
+            {
+                {
+                    if (diak.nev == neve)
+                        Console.WriteLine("Átlaga:" + diak.jegyek.Average());
+                    megvan=true;
+                }
+            }
+            if (megvan==false)
+                Console.WriteLine("Nincs ilyen nevű diák!");
 
         }
     }
